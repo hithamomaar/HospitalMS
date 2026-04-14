6010ca9839959fdb06f4ef77bd453be3b037b35a
hitham <hitham.a1000@gmail.com>
2026-04-14 15:30:41 +0200

    Add MVC web layer connected to EF Core

--- Hospital/Configurations/HospitalContext.cs
+++ Hospital/Configurations/HospitalContext.cs
@@ -16,11 +16,14 @@ namespace Hospital.Configurations
         public DbSet<DoctorSchedule> DoctorSchedules { get; set; }
         public DbSet<Medication> Medications { get; set; }
         public DbSet<Prescription> Prescriptions { get; set; }
-
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        public HospitalContext(DbContextOptions<HospitalContext> options) : base(options)
         {
-            optionsBuilder.UseSqlServer("Server=.;Database=HospitalMSDB;Trusted_Connection=True;TrustServerCertificate=True;");
         }
+
+        //protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        //{
+        //    optionsBuilder.UseSqlServer("Server=.;Database=HospitalMSDB;Trusted_Connection=True;TrustServerCertificate=True;");
+        //}
         protected override void OnModelCreating(ModelBuilder builder)
         {
             builder.ApplyConfiguration(new UserConfiguration());
@@ -32,6 +35,13 @@ namespace Hospital.Configurations
             builder.ApplyConfiguration(new MedicationConfiguration());
             builder.ApplyConfiguration(new PrescriptionConfiguration());
 
+            builder.Entity<User>().HasQueryFilter(x => !x.IsDeleted);
+            builder.Entity<Specialty>().HasQueryFilter(x => !x.IsDeleted);
+            builder.Entity<Appointment>().HasQueryFilter(x => !x.IsDeleted);
+            builder.Entity<DoctorSchedule>().HasQueryFilter(x => !x.IsDeleted);
+            builder.Entity<Medication>().HasQueryFilter(x => !x.IsDeleted);
+            builder.Entity<Prescription>().HasQueryFilter(x => !x.IsDeleted);
+
             base.OnModelCreating(builder);
         }
         public override int SaveChanges()
@@ -41,8 +51,8 @@ namespace Hospital.Configurations
                 if (entry.State == EntityState.Unchanged) continue;
                 if (entry.State == EntityState.Detached) continue;
 
-                entry.Entity.LastModified = DateTime.UtcNow
-                else if (entry.State == EntityState.Deleted)
+                entry.Entity.LastModified = DateTime.UtcNow;
+                if (entry.State == EntityState.Deleted)
                 {
                     entry.State = EntityState.Modified;
                     entry.Entity.IsDeleted = true;
